7b685356bcd916559c78b99dfefc879ec5037741
Oluwatobi Fatumo <tobyfatumo@gmail.com>
2019-12-02 01:56:33 +0100

    social update

---
+++ index.js
@@ -0,0 +1,21 @@
+let count = new Date("dec 25,2019 00:00:00").getTime();
+
+let x = setInterval(() => {
+let now = new Date().getTime();
+let d = count - now;
+
+let days = Math.floor(d/(1000*60*60*24));
+let hours = Math.floor((d%(1000*60*60*24))/(1000*60*60));
+let minutes = Math.floor((d%(1000*60*60))/(1000*60));
+let seconds = Math.floor((d%(1000*60))/1000);
+
+document.getElementById("days").innerHTML = days;
+document.getElementById("hours").innerHTML = hours;
+document.getElementById("minutes").innerHTML = minutes;
+document.getElementById("seconds").innerHTML = seconds;
+
+if(d<= 0){
+    clearInterval(x)
+}
+
+},1000);
